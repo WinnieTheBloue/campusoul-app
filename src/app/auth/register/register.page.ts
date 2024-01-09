03433ba0745dc8d62ff2e6c62c988fa9b40d39a6
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-01-09 15:09:23 +0100

    style

--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -8,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterPage implements OnInit {
   email?: string = ""
   password?: string = ""
-  
+  confirmPassword?: string = ""
+
   constructor() { }
 
   ngOnInit() {
