fc5157253037293fb4c861b1892bfb7d02331a89
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-01-16 13:33:11 +0100

    Chat front

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -6,7 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.page.scss'],
 })
 export class ChatPage implements OnInit {
-
+  matches: any[] = [ 
+    {
+      name: 'Megan Fox',
+      id: '1',
+      img: 'https://hips.hearstapps.com/hmg-prod/images/gettyimages-843456920.jpg',
+      lastMsg: 'Hey, how are you?',
+      lastMsgSender: '1'
+    },
+    {
+      name: 'Ice Spice',
+      id: '2',
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Ice_Spice_Next_Wav_by_Keinoflo_uploaded_by_James_Tamim_V4_%28cropped_V2%29_2022.png/640px-Ice_Spice_Next_Wav_by_Keinoflo_uploaded_by_James_Tamim_V4_%28cropped_V2%29_2022.png',
+      lastMsg: 'Hey, how are you?',
+      lastMsgSender: '9'
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/chat/chatroom/chatroom.page.ts
+++ src/app/chat/chatroom/chatroom.page.ts
@@ -6,10 +6,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chatroom.page.scss'],
 })
 export class ChatroomPage implements OnInit {
+  match: any = {
+    name: 'Megan Fox',
+    id: '1',
+    img: 'https://hips.hearstapps.com/hmg-prod/images/gettyimages-843456920.jpg',
+  }
+  message?: string;
+  userId: string = '9';
+  messages: any[] = [
+    {
+      sender: '1',
+      msg: 'Hey, how are you?'
+    },
+    {
+      sender: '9',
+      msg: 'I\'m fine, thanks. And you?'
+    },
+    {
+      sender: '1',
+      msg: 'I\'m fine too, thanks for asking.'
+    },
+    {
+      sender: '9',
+      msg: 'So, what are you doing tonight?'
+    },
+    {
+      sender: '1',
+      msg: 'Nothing much, just watching TV. You?'
+    },
+    {
+      sender: '9',
+      msg: 'I\'m going to the movies with some friends.'
+    },
+    {
+      sender: '9',
+      msg: 'Do you want to come?'
+    },
+    {
+      sender: '1',
+      msg: 'Sure, why not?'
+    },
+    {
+      sender: '9',
+      msg: 'Great! I\'ll pick you up at 8.'
+    },
+    {
+      sender: '1',
+      msg: 'See you later.'
+    },
+    {
+      sender: '9',
+      msg: 'Bye.'
+    }
 
+  ]
   constructor() { }
 
   ngOnInit() {
   }
 
+  sendMessage() {
+    console.log(this.message);
+  }
 }
