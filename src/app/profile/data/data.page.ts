9574b96f6f792b87575965561b2a3ac4d1529f1c
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-01-16 11:16:39 +0100

    Profile front

--- src/app/profile/data/data.page.ts
+++ src/app/profile/data/data.page.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data.page.scss'],
 })
 export class DataPage implements OnInit {
-
+  profile: any = {
+    name: 'John Doe',
+    birthdate: '1990-02-17',
+    email: 'john@doe.ch',
+    bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec vel libero at lectus rutrum vestibulum vitae ut turpis. Ut ultricies pulvinar posuere. Nulla rutrum, libero nec pharetra accumsan, enim leo blandit dui, ac bibendum augue dui sed justo.',
+  }
   constructor() { }
 
   ngOnInit() {
--- src/app/profile/interests/interests.page.ts
+++ src/app/profile/interests/interests.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+interface Interest {
+  id: string;
+  name: string;
+}
 
 @Component({
   selector: 'app-interests',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./interests.page.scss'],
 })
 export class InterestsPage implements OnInit {
-
+  interests: Interest[] = [
+    {id:"1", name:"Ski"},
+    {id:"2", name:"Snowboard"},
+    {id:"3", name:"Hiking"},
+    {id:"4", name:"Camping"},
+    {id:"5", name:"Biking"},
+    {id:"6", name:"Kayaking"},
+    {id:"7", name:"Running"},
+    {id:"8", name:"Swimming"},
+    {id:"9", name:"Tennis"},
+  ]
+  selectedInterests: Interest[] = [{id:"1", name:"Ski"}, {id:"5", name:"Biking"}]
+  selectedSum?: number = 2
   constructor() { }
 
   ngOnInit() {
   }
 
+  isSelected(interest: Interest): boolean {
+    return this.selectedInterests.some(selectedInterest => selectedInterest.id === interest.id);
+  }
 }
