3f004994e07a3e05527843406dbd6fa12ea46bc6
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-01-23 15:21:53 +0100

    modal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { OverlayEventDetail } from '@ionic/core/components';
 import { PhotoService } from '../services/photo.service';
 import { UserService } from '../services/user.service';
 import { AuthService } from '../services/auth.service';
@@ -9,27 +11,76 @@ import { AuthService } from '../services/auth.service';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
   userId: any = ""
   users: any = []
   noMoreUsers: boolean = false
+  ageMin: number = 18;
+  ageMax: number = 30;
+  distanceMax: number = 20;
 
-
-  constructor(private photoService: PhotoService, private userService: UserService, private authService: AuthService) { }
+  constructor(private modal: ModalController, private photoService: PhotoService, private userService: UserService, private authService: AuthService) { }
   ngOnInit() {
-    // this.userId = this.authService.getId();
-    this.userService.getAllUsers(1, 0, 0, 0).subscribe(
+    if (localStorage.getItem('ageMin')) {
+      this.ageMin = Number(localStorage.getItem('ageMin'))
+    }
+    if (localStorage.getItem('ageMax')) {
+      this.ageMax = Number(localStorage.getItem('ageMax'))
+    }
+    if (localStorage.getItem('distanceMax')) {
+      this.distanceMax = Number(localStorage.getItem('distanceMax'))
+    }
+
+    this.loadUsers();
+
+  }
+
+  loadUsers() {
+    console.log('Chargement des utilisateurs')
+    this.userService.getAllUsers(1, this.ageMin, this.ageMax, this.distanceMax).subscribe(
       (response) => {
         this.users = response.users;
         this.userId = this.users[0]._id;
+        if (this.users.length == 0) {
+          this.noMoreUsers = true
+        }
       },
       (error) => {
         console.error('Erreur lors du chargement des données utilisateur:', error);
       }
     );
-    if(this.users.length == 0){
-      this.noMoreUsers = true
+  }
+
+
+  cancel() {
+    this.modal.dismiss(null, 'cancel');
+  }
+
+  confirm() {
+    this.modal.dismiss(null, 'confirm');
+
+  }
+
+  onRangeChange(event: any) {
+    this.distanceMax = event.detail.value;
+  }
+  pinFormatter(value: number) {
+    return `${value} km`;
+  }
+  onWillDismiss(event: Event) {
+    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+    if (ev.detail.role === 'confirm') {
+      if (this.ageMin) {
+        localStorage.setItem('ageMin', this.ageMin.toString())
+      }
+      if (this.ageMax) {
+        localStorage.setItem('ageMax', this.ageMax.toString())
+      }
+      if (this.distanceMax) {
+        localStorage.setItem('distanceMax', this.distanceMax.toString())
+      }
+
+      this.loadUsers();
     }
   }
-  
+
 }
