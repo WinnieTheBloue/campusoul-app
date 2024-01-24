79249ee1779d6a7a88dd2bd20356c745a3f05219
WinnieTheBloue <pablo.crotti@heig-vd.ch>
2024-01-24 19:43:19 +0100

    notif

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 import { Observable, Subject } from 'rxjs';
 
 @Injectable({
@@ -8,7 +9,7 @@ export class WebSocketService {
   private socket: WebSocket;
   private messagesSubject: Subject<any> = new Subject();
 
-  constructor() {
+  constructor(private toastController: ToastController) {
     this.socket = new WebSocket('wss://campusoul-hrim.onrender.com');
 
     this.socket.addEventListener('open', (event) => {
@@ -17,12 +18,25 @@ export class WebSocketService {
 
     this.socket.addEventListener('message', (event) => {
       const message = JSON.parse(event.data);
+      // console.log('Message reçu : ', message.newChatMessage.content);
+      this.presentToast(message.newChatMessage.content);
       this.messagesSubject.next(message);
     });
 
     this.socket.addEventListener('close', (event) => {
       console.log('WebSocket fermé');
     });
+  }
+
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 1500,
+      position: 'top',
+    });
+
+    await toast.present();
   }
 
   getMessages(): Observable<any> {
